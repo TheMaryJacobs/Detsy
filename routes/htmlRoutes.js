e8377f9b938170a7104b4a72448890b788da7403
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2020-01-08 10:29:40 -0600

    fixing routing on htmlrouting.js and linking html documents to css

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,21 +3,21 @@ const path = require("path");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
   });
   app.get("/add-product", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/add-product.html"));
+    res.sendFile(path.join(__dirname, "/add-product.html"));
   });
   app.get("/messaging", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/messaging.html"));
+    res.sendFile(path.join(__dirname, "/messaging.html"));
   });
   app.get("/product-page", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/product-page.html"));
+    res.sendFile(path.join(__dirname, "/product-page.html"));
   });
   app.get("/sign-up", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/sign-up.html"));
+    res.sendFile(path.join(__dirname, "/sign-up.html"));
   });
   app.get("/user-page", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/user-page.html"));
+    res.sendFile(path.join(__dirname, "/user-page.html"));
   });
 };
