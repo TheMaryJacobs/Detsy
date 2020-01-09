5b6be9e8e445b4ba1207126b1f75201680bb1f9a
amira <chikhaoui.amira07@gmail.com>
2020-01-09 13:02:55 -0600

    update models products and user and api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,6 +5,8 @@ var formidable = require("formidable");
 // using passport for authentication
 var passport = require("../config/passport");
 
+var bCrypt = require('bcrypt-nodejs');
+
 // Requiring our models
 var db = require("../models");
 var path = require("path");
@@ -59,7 +61,9 @@ module.exports = function(app) {
       });
     }
   });
+  /**************************** 
   // get all products
+  ***************************/
 
   app.get("/api/products/all", function(req, res) {
     db.Product.findAll({}).then(function(dbProducts) {
@@ -77,13 +81,13 @@ module.exports = function(app) {
       res.json(results);
     });
   });
-
+/**************************** 
   // Add a New Products
+  ***************************/
   app.post("/addProducts", function(req, res) {
     // Setup formidable
     // Instantiate a new formidable form for processing.
     var form = new formidable.IncomingForm();
-
     // Parse the form request
     // form.parse analyzes the incoming stream data, picking apart the different fields and files .
     form.parse(req, function(err, fields, files) {
@@ -112,13 +116,13 @@ module.exports = function(app) {
       console.log("File Uploaded succesfully");
     });
   });
-  
+  /**************************** 
   // PUT route for updating products
+  ****************************/
   app.put('/editProducts/:id', function(req, res) {
     //req.isAuthenticated() will return true if user is logged in
     // to check if a user is authenticated or not we use this method.
     if(req.isAuthenticated()) {
-
       db.Product.update({
         productName: req.body.edited_product_name,
         price: req.body.edited_price,
@@ -132,10 +136,117 @@ module.exports = function(app) {
         console.log(req.params.id);
         res.redirect('/editProducts/' + req.params.id)
       });
+/**************************** 
+ Updates product image 
+ ****************************/
+
+      app.put("/product-image", function(req, res) {
+
+        console.log(req.body.id);
+        // Setup formidable
+        var form = new formidable.IncomingForm();
+    
+        // Parse the form request
+        form.parse(req, function(err, fields, files) {
+    
+          // Add profile image to DB    
+          db.Product.update({
+            imageURL: files.edited_imageURL.name
+          }, {
+            where: {
+              id: fields.id
+            }
+          }).then(function(dbUser) {  
+            res.redirect('/editProducts/' + fields.id);
+          });
+        });
+    
+        form.on('fileBegin', function (name, file){
+          file.path = path.basename(path.dirname('../')) + '/public/uploads/products/' + file.name;     
+        });
+    
+        form.on('end', function() {
+          console.log('File Uploaded succefully');
+        });
+      });
+/**************************** 
+// PUT route for deleting products
+***************************/
+      app.delete("/api/products/:id", function(req, res) {
+        db.Product.destroy({
+          where: {
+            id: req.params.id
+          }
+        }).then(function(results) {
+          res.json(results);
+        });
+      });
+    // PUT route for deleting products from the specific user 
+      app.delete("/products/delete/:id", function(req, res) {
+        db.Product.destroy({
+          where: {
+            id: req.params.id
+          },
+          include: [db.User]
+        }).then(function(results) {
+          res.redirect("/edit-profile");
+        });
+      });
 
     }
   });
+/**************************** 
+// Put route to login 
+***************************/
+  app.get("/api/login", function(req, res) {
+    db.User.findOne({
+      where: {
+        $and: [
+          {userName: req.params.userName }, // AND ( userName=userName )
+          {password: req.params.password }  // AND ( password=password )
+        ]
+      }
+    }).then(function(results) {
+      res.json(results);
+    }).catch(function (err) {
+      res.json("");
+    }); 
+  });
+/**************************** 
+ // PUT route for Updating User.
+***************************/
 
+/**************************** 
+  // Add a profile image 
+***************************/
+  app.put("/profile-image", function(req, res) {
+    // Setup formidable
+    var form = new formidable.IncomingForm();
+
+    // Parse the form request
+    form.parse(req, function(err, fields, files) {
+
+      // Add profile image to DB    
+      db.User.update({
+        profileImage: files.profile_image.name
+      }, {
+        where: {
+          id: fields.id
+        }
+      }).then(function(dbUser) {  
+        res.redirect('/store/' + req.user.userName );
+      });
+    });
+
+    form.on('fileBegin', function (name, file){
+      // path.basename() methods returns the last portion of the path
+      file.path = path.basename(path.dirname('../')) + '/public/uploads/users/' + file.name;     
+    });
+
+    form.on('end', function() {
+      console.log('File Uploaded Succefully');
+    });
+  });
 
 
 };
